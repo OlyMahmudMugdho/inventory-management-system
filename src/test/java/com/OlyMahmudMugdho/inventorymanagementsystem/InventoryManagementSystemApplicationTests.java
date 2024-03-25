176d62b05192c12bbc8c4bda65586c16733b1069
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-25 22:39:28 +0600

    init setup

---
+++ src/test/java/com/OlyMahmudMugdho/inventorymanagementsystem/InventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
