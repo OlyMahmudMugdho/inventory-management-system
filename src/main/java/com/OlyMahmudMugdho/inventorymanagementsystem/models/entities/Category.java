46a20d9620db886497a3ba82e79f361d5896ca77
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-26 13:04:24 +0600

    added Category entity

---
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/entities/Category.java
@@ -0,0 +1,20 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem.models.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "categories")
+public class Category {
+    @Id
+    @Column(name = "category_id")
+    private Long categoryId;
+    @Column(name = "category_name")
+    private String categoryName;
+}
