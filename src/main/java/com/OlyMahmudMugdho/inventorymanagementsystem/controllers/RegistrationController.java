78282452a2dbd47bfe72bf6d2c32bdc46e72f967
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-28 21:44:11 +0600

    added RegistrationController

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/RegistrationController.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/RegistrationController.java
@@ -1,15 +1,29 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.controllers;
 
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.RegistrationForm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/register")
 public class RegistrationController {
+    private static final Logger log = LoggerFactory.getLogger(RegistrationController.class);
+
     @GetMapping("")
     public String registerForm(Model model) {
+        model.addAttribute("registrationForm", new RegistrationForm());
         return "registration";
+    }
+
+    @PostMapping("")
+    public String register(@ModelAttribute("registrationForm") RegistrationForm registerForm, Model model){
+        System.out.println(registerForm);
+        return "redirect:/";
     }
 }
