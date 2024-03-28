cd78a28592b4e4398ec0e80d714748a6d573e355
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-28 21:52:20 +0600

    added UserService interface

---
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/services/IUserService.java
@@ -0,0 +1,7 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem.services;
+
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.User;
+
+public interface IUserService {
+    public User addUser(User user);
+}
