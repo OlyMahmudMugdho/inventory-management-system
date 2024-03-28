9b08cec9a7f0ec40109105514fd2fabaec3348bd
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-28 21:27:12 +0600

    added User repository

---
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem.repositories;
+
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
