fd766c16c27228e91bd583e60f802fe6e960c7d7
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-28 21:52:48 +0600

    implemented UserService

---
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/services/impl/UserService.java
@@ -0,0 +1,20 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem.services.impl;
+
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.User;
+import com.OlyMahmudMugdho.inventorymanagementsystem.repositories.UserRepository;
+import com.OlyMahmudMugdho.inventorymanagementsystem.services.IUserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements IUserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public User addUser(User user) {
+        userRepository.save(user);
+        return user;
+    }
+}
