b9b481ce62da2ca75be6f1767f24f7a51d868832
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-29 02:07:27 +0600

    updated SecurityConfig

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/configs/SecurityConfig.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/configs/SecurityConfig.java
@@ -1,14 +1,52 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.configs;
 
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.User;
+import com.OlyMahmudMugdho.inventorymanagementsystem.repositories.UserRepository;
+import com.OlyMahmudMugdho.inventorymanagementsystem.services.impl.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig  {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> {
+            User user = userRepository.findByUsername(username);
+            System.out.println(user);
+            if (user != null){
+                return user;
+            }
+            throw new UsernameNotFoundException("User not found");
+        };
+    }
+
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
