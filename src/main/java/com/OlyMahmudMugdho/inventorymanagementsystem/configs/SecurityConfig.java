283d2e0e01f70b47c9b8a3645c16d02ba142b5e3
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-28 15:40:06 +0600

    support for static files during login

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/configs/SecurityConfig.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/configs/SecurityConfig.java
@@ -1,22 +1,30 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.configs;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
         httpSecurity
                 .authorizeHttpRequests((request) -> {
                     request
+                            .requestMatchers("/resources/**", "/js/**", "/styles/**","/css/**").permitAll()
+                            .requestMatchers("/login").permitAll()
+                            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                             .anyRequest().authenticated();
+
                 })
                 .formLogin( (form) -> {
                     form.loginPage("/login")
+                            .successForwardUrl("/")
                     .permitAll();
                 } );
 
