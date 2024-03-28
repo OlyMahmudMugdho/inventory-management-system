99e5b75961ec006da052550e7469ad2a2153885e
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-28 14:49:03 +0600

    custom login page

---
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/configs/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests((request) -> {
+                    request
+                            .anyRequest().authenticated();
+                })
+                .formLogin( (form) -> {
+                    form.loginPage("/login")
+                    .permitAll();
+                } );
+
+        return httpSecurity.build();
+    }
+}
---
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/LoginController.java
@@ -0,0 +1,7 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+}
