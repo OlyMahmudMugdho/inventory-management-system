2a74e49cc8987b72f65133ae91fa7a7e401ee805
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-28 21:26:05 +0600

    added RegisterForm model

---
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/RegisterForm.java
@@ -0,0 +1,16 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem.models;
+
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.User;
+import lombok.Data;
+
+@Data
+public class RegisterForm {
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+
+    public User toUser(String name, String username, String password, String email){
+        return new User(name,username,password,email);
+    }
+}
