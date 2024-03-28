85f0a2f8263aaee52494afdf033107662bf3abaa
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-29 02:05:26 +0600

    updated UserRepository

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/UserRepository.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/UserRepository.java
@@ -2,7 +2,8 @@ package com.OlyMahmudMugdho.inventorymanagementsystem.repositories;
 
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
 }
