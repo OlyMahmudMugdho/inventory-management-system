3bfcb0d0a8c6fde97ec7fccd23179e4b342afb02
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-29 21:45:32 +0600

    identifying authenticated user

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/LoginController.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/LoginController.java
@@ -1,5 +1,7 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.controllers;
 
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.User;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +13,16 @@ import java.security.Principal;
 @RequestMapping("/login")
 public class LoginController {
     @GetMapping("")
-    public String login(Principal principal) {
-        if (principal != null) {
-            System.out.println(principal);
+    public String login(Authentication authentication) {
+        if (authentication != null) {
+            User user = (User) authentication.getPrincipal();
+            System.out.println(user.getEmail());
             return "redirect:/";
         }
+//        if (principal != null) {
+//            System.out.println(principal);
+//            return "redirect:/";
+//        }
         return "login";
     }
 }
