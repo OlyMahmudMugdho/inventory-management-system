caaef6438a08bc402f66a24e7352a874bb405806
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-29 12:28:20 +0600

    restricted login form for authenticated user

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/LoginController.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/LoginController.java
@@ -1,9 +1,21 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.security.Principal;
+
 @Controller
+@RequestMapping("/login")
 public class LoginController {
+    @GetMapping("")
+    public String login(Principal principal) {
+        if (principal != null) {
+            System.out.println(principal);
+            return "redirect:/";
+        }
+        return "login";
+    }
 }
