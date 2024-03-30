710b26f557bbb454d37503ce2545d9e0eaaa0b89
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-30 21:41:22 +0600

    updated RegistrationForm model

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/RegistrationForm.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/RegistrationForm.java
@@ -4,15 +4,18 @@ import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.User;
 import lombok.Data;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Set;
+
 @Data
 public class RegistrationForm {
     private String name;
     private String username;
     private String password;
     private String email;
+    private Set<Role> roles;
 
-    public User toUser(PasswordEncoder passwordEncoder){
+    public User toUser(PasswordEncoder passwordEncoder, Set<Role> roles){
         System.out.println(this.username + " matched");
-        return new User(name,username,passwordEncoder.encode(password),email);
+        return new User(name,username,passwordEncoder.encode(password),email, roles);
     }
 }
