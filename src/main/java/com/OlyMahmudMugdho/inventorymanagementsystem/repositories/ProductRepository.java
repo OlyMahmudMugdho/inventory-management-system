2120d2e23f7862a8198e7e74aa2d1affc0cb82ff
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-30 21:02:43 +0600

    added ProductRepository

---
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem.repositories;
+
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
