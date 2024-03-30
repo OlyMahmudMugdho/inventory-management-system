7b988d54ab4e2aa81a48c13b8654af70e7abff2e
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-30 21:21:55 +0600

    added RoleRepository

---
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem.repositories;
+
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
+
