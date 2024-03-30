8a0a8245a785736de611c0d09d2f409d725369c3
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-30 21:41:55 +0600

    RoleRepository bug fixed

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/RoleRepository.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/RoleRepository.java
@@ -4,6 +4,6 @@ import com.OlyMahmudMugdho.inventorymanagementsystem.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
+    Role findByAuthority(String authority);
 }
 
