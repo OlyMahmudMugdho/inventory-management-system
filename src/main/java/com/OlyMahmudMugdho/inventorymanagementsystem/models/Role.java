221aebcd6b749cd84d78ad193fd02efc6446457e
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-30 22:23:16 +0600

    fixed Role entity

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/Role.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/Role.java
@@ -1,8 +1,6 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
