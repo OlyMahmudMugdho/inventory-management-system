b573179b4db5c6451b67d2d3889165e8bce26e33
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-31 23:06:32 +0600

    DTO to entity

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/services/ProductService.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/services/ProductService.java
@@ -1,8 +1,22 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.services;
 
+import com.OlyMahmudMugdho.inventorymanagementsystem.mappers.impl.ProductMapper;
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.dto.ProductDto;
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Product;
+import com.OlyMahmudMugdho.inventorymanagementsystem.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
 
+    public ProductService(ProductRepository productRepository, ProductMapper productMapper) {
+        this.productRepository = productRepository;
+        this.productMapper = productMapper;
+    }
+
+    public Product addProduct(ProductDto productDto) {
+        return productRepository.save(productMapper.mapFrom(productDto));
+    }
 }
