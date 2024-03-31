d6e5330d3d1c3522ecc2753549d6b56011546dbe
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-31 21:49:58 +0600

    updated Product entity

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/entities/Product.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/entities/Product.java
@@ -2,12 +2,14 @@ package com.OlyMahmudMugdho.inventorymanagementsystem.models.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "products")
