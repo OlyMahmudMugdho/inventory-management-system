e278ca64a9ae45db8acfb6a20bce3875e4886a12
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-31 23:04:02 +0600

    added MapperConfig

---
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/configs/MapperConfig.java
@@ -0,0 +1,15 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem.configs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
