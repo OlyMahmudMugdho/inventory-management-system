20115704b54a738cb5f7df59266441d7c95b29e1
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-31 21:39:59 +0600

    added link to add Product

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/configs/MvcConfig.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/configs/MvcConfig.java
@@ -11,5 +11,6 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/products/add-product").setViewName("products/add-product-form");
     }
 }
