ce2ee00c26e7604a13c3890f49c1fd2e46c79c9f
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-04-01 20:11:59 +0600

    added dto support in product service

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/services/ProductService.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/services/ProductService.java
@@ -6,6 +6,9 @@ import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Product;
 import com.OlyMahmudMugdho.inventorymanagementsystem.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
@@ -18,5 +21,16 @@ public class ProductService {
 
     public Product addProduct(ProductDto productDto) {
         return productRepository.save(productMapper.mapFrom(productDto));
+    }
+
+    public List<ProductDto> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        List<ProductDto> productDtos = new ArrayList<>();
+        if (!products.isEmpty()) {
+            for (Product product : products) {
+                productDtos.add(productMapper.mapTo(product));
+            }
+        }
+        return productDtos;
     }
 }
