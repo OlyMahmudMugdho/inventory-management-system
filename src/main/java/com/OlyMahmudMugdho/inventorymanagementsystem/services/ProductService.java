f949711946e47ca571dddbbefbc9ee5fb8f37e38
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-04-05 12:08:48 +0600

    added product edit feature

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/services/ProductService.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/services/ProductService.java
@@ -4,6 +4,7 @@ import com.OlyMahmudMugdho.inventorymanagementsystem.mappers.impl.ProductMapper;
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.dto.ProductDto;
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Product;
 import com.OlyMahmudMugdho.inventorymanagementsystem.repositories.ProductRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,10 +15,12 @@ import java.util.Optional;
 public class ProductService {
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final ModelMapper modelMapper;
 
-    public ProductService(ProductRepository productRepository, ProductMapper productMapper) {
+    public ProductService(ProductRepository productRepository, ProductMapper productMapper, ModelMapper modelMapper) {
         this.productRepository = productRepository;
         this.productMapper = productMapper;
+        this.modelMapper = modelMapper;
     }
 
     public Product addProduct(ProductDto productDto) {
@@ -39,4 +42,12 @@ public class ProductService {
         Optional<Product> product = productRepository.findById(id);
         return product.map(productMapper::mapTo);
     }
+
+    public ProductDto editProduct(ProductDto productDto) {
+            Product product = productMapper.mapFrom(productDto);
+            productRepository.save(product);
+            return productMapper.mapTo(product);
+    }
+
+
 }
