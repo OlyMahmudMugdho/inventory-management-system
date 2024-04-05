d184ea03b4e6479c3ccc3eb65934f6107047db0a
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-04-05 17:07:23 +0600

    updated Product repo

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/ProductRepository.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/ProductRepository.java
@@ -4,5 +4,5 @@ import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    boolean deleteByProductId(long id);
 }
