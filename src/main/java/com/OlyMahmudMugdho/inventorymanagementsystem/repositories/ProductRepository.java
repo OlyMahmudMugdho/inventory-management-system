01e25a1a353b9f4f47958004dcea92eb3532f8a1
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-04-06 22:09:00 +0600

    updated Product Repository for pagination

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/ProductRepository.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/ProductRepository.java
@@ -1,8 +1,13 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.repositories;
 
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean deleteByProductId(long id);
+
+    @Override
+    Page<Product> findAll(Pageable pageable);
 }
