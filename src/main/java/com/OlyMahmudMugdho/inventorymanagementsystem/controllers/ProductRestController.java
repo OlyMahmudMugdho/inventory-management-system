301c68f17a7d887f6ef2043efa23c853e5a2352f
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-04-10 13:25:26 +0600

    tested product rest controller

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/ProductRestController.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/ProductRestController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,4 +33,12 @@ public class ProductRestController {
         return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
         //return "requested " + size + " page = " + page;
     }
+
+//    @GetMapping("/search")
+//    public ResponseEntity<String> searchProduct(@RequestParam(defaultValue = " ") String keyword, Model model) {
+//        List<Product> products = productRepository.search(keyword);
+//        System.out.println(products);
+//        return new ResponseEntity<>(products.getFirst().toString(), HttpStatus.OK);
+//    }
+
 }
