b6a0dd83eaf4c7bd6540adfd18e51b9b817fee73
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-04-10 13:13:32 +0600

    added search func. in repo

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/ProductRepository.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/ProductRepository.java
@@ -4,10 +4,16 @@ import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    boolean deleteByProductId(long id);
 
     @Override
     Page<Product> findAll(Pageable pageable);
+
+    @Query(value = "SELECT p FROM products p WHERE "
+    + "p.name LIKE CONCAT('%', :keyword,'%') ")
+    Page<Product> search(String keyword, Pageable pageable);
 }
