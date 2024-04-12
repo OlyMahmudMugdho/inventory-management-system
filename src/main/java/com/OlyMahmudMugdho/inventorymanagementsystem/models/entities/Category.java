a250837e9c113d0025932e66fc025d938681e090
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-04-13 00:51:02 +0600

    upadted category entity

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/entities/Category.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/entities/Category.java
@@ -1,8 +1,6 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.models.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Entity(name = "categories")
 public class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "category_id")
     private Long categoryId;
 
