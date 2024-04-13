150010e7e21254d905ff9fb61b05a302b8f4664e
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-04-13 14:10:00 +0600

    pagination feature in Category repo

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/CategoryRepository.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/CategoryRepository.java
@@ -1,8 +1,12 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.repositories;
 
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByCategoryId(Long categoryId);
+
+    Page<Category> findAll(Pageable pageable);
 }
