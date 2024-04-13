7fb2014acf2ada81411233e1042048b76cc547c2
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-04-13 14:12:28 +0600

    pagination feature in Category controller

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/CategoryController.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/CategoryController.java
@@ -2,16 +2,22 @@ package com.OlyMahmudMugdho.inventorymanagementsystem.controllers;
 
 import com.OlyMahmudMugdho.inventorymanagementsystem.mappers.impl.CategoryMapper;
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.dto.CategoryDto;
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.dto.ProductDto;
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Category;
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Product;
 import com.OlyMahmudMugdho.inventorymanagementsystem.services.CategoryService;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/categories")
@@ -23,6 +29,32 @@ public class CategoryController {
     CategoryController(CategoryService categoryService, CategoryMapper categoryMapper) {
         this.categoryService = categoryService;
         this.categoryMapper = categoryMapper;
+    }
+
+    @GetMapping("")
+    public String getAllCategories(Model model,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        Pageable pagination = PageRequest.of(page, size);
+        Page<Category> categories = categoryService.getPaginatedCategories(pagination);
+        List<CategoryDto> categoryDtos = new ArrayList<>();
+        for (Category category : categories.getContent()) {
+            categoryDtos.add(categoryMapper.mapTo(category));
+        }
+        List<Integer> pageNumbers = new ArrayList<>();
+        for (int i = 0; i < categories.getTotalPages(); i++) {
+            pageNumbers.add(i);
+        }
+        model.addAttribute("categories", categoryDtos);
+        model.addAttribute("pagination", pagination);
+        model.addAttribute("currentPage", page);
+        System.out.println("total page = " + categories.getTotalPages());
+        model.addAttribute("totalPages", categories.getTotalPages());
+        model.addAttribute("pageNumbers", pageNumbers);
+        model.addAttribute("lastPage", categories.getTotalPages() - 1);
+        model.addAttribute("size",size);
+        return "categories/all-category-page";
     }
 
     @GetMapping("/add-category")
