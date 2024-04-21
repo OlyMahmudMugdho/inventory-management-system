ff80e02c314779b3a4320702a71f0b1f44bd916f
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-04-21 13:12:26 +0600

    added role controller

---
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/RoleController.java
@@ -0,0 +1,33 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem.controllers;
+
+import com.OlyMahmudMugdho.inventorymanagementsystem.mappers.impl.RoleMapper;
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.Role;
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.dto.RoleDto;
+import com.OlyMahmudMugdho.inventorymanagementsystem.services.RoleService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/roles")
+public class RoleController {
+    private final RoleService roleService;
+    private RoleMapper roleMapper;
+
+    public RoleController(RoleService roleService, RoleMapper roleMapper) {
+        this.roleService = roleService;
+        this.roleMapper = roleMapper;
+    }
+
+    @GetMapping
+    public String rolesPage(Model model) {
+        List<Role> fetchedRoles = roleService.getAllRoles();
+        List<RoleDto> roles = fetchedRoles.stream().map(r -> roleMapper.mapTo(r)).toList();
+        model.addAttribute("roles", roles);
+        return "roles/all-roles-page";
+    }
+}
