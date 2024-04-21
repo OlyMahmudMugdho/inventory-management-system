4544b2d11174b00040c32200518ea1ab3ba138dd
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-04-21 13:40:04 +0600

    adding role feature

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/RegistrationForm.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/RegistrationForm.java
@@ -1,12 +1,16 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.models;
 
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.User;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Set;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegistrationForm {
     private String name;
     private String username;
