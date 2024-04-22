ea0bcbf0618ff540290e46688ebb6656a3bc7a81
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-04-22 12:20:52 +0600

    create USER role while startup

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/InventoryManagementSystemApplication.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/InventoryManagementSystemApplication.java
@@ -1,13 +1,33 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem;
 
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.Role;
+import com.OlyMahmudMugdho.inventorymanagementsystem.repositories.RoleRepository;
+import com.OlyMahmudMugdho.inventorymanagementsystem.services.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryManagementSystemApplication {
+	@Autowired
+	private final RoleService roleService;
+
+	public InventoryManagementSystemApplication(RoleService roleService) {
+		this.roleService = roleService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagementSystemApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner startup() {
+		return args -> {
+			Role role = new Role(1,"USER");
+			roleService.createRole(role);
+			System.out.println("role created");
+		};
+	}
 }
