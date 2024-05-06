6cf5902feaf353b2fb92852e10553b9e06395767
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-06 12:23:18 +0600

    removed unused imports

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/GlobalController.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/GlobalController.java
@@ -1,10 +1,6 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.controllers;
 
-import com.OlyMahmudMugdho.inventorymanagementsystem.services.CategoryService;
 import com.OlyMahmudMugdho.inventorymanagementsystem.services.GlobalService;
-import com.OlyMahmudMugdho.inventorymanagementsystem.services.ProductService;
-import com.OlyMahmudMugdho.inventorymanagementsystem.services.RoleService;
-import com.OlyMahmudMugdho.inventorymanagementsystem.services.impl.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
