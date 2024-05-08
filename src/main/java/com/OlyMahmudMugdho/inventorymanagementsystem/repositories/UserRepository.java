c81b89aa4531059571191893eee41289441579c4
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-08 15:46:48 +0600

    added Pageable findAll()

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/UserRepository.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.repositories;
 
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
+    Page<User> findAll(Pageable pageable);
 }
