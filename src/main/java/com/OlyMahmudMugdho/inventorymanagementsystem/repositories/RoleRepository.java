49ecfc6a73aebbcfcd198276f6e0691a8aaa6dd4
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-08 15:48:48 +0600

    removed unused method

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/RoleRepository.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/repositories/RoleRepository.java
@@ -1,7 +1,6 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.repositories;
 
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.Role;
-import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
