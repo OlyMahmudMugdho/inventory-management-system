ea0bd07bc9100c81113e9b02d782a289d45f5d45
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-08 22:02:12 +0600

    route config changed

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/configs/SecurityConfig.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/configs/SecurityConfig.java
@@ -7,6 +7,7 @@ import jakarta.servlet.DispatcherType;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
