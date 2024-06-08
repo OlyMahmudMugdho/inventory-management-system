6c13cf63851653579cf79c61e1b7b4b54a5fdaac
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-08 22:40:40 +0600

    getProductById

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/rest/ProductControllerRest.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/rest/ProductControllerRest.java
@@ -6,11 +6,13 @@ import com.OlyMahmudMugdho.inventorymanagementsystem.models.dto.rest.ProductDtoR
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Product;
 import com.OlyMahmudMugdho.inventorymanagementsystem.services.ProductService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -28,6 +30,16 @@ public class ProductControllerRest {
         List<ProductDtoRest> products = new ArrayList<>();
         productDtos.stream().map(p -> productMapper.mapToRest(p)).forEach(products::add);
         return products;
+    }
+
+    @GetMapping(path = {"/products/{id}","/products/{id}/"})
+    public Optional<ProductDtoRest> getProductById(@PathVariable Long id){
+        Optional<ProductDto> productDto = productService.getProductById(id);
+        if(productDto.isPresent()){
+            ProductDtoRest productDtoRest = productMapper.mapToRest(productDto.get());
+            return Optional.of(productDtoRest);
+        }
+        return Optional.empty();
     }
 
 }
