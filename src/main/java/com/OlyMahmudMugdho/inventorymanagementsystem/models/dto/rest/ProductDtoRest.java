078ba4f62eee0796393346df814b2a28da9be70a
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-08 22:16:30 +0600

    added product dto for rest

---
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/dto/rest/ProductDtoRest.java
@@ -0,0 +1,20 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem.models.dto.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDtoRest {
+    private int productId;
+    private String code;
+    private String name;
+    private String description;
+    private int stocks;
+    private double price;  // Assuming price should be a double
+    private double unitPrice; // Assuming unitPrice should be a double
+    private String status;
+    private String categories;
+}
