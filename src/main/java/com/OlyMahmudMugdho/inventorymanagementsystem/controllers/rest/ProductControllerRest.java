afe9dbcf62cab116e021235cb685151cf85ea0f8
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-08 22:03:20 +0600

    Rest Resource

---
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/rest/ProductControllerRest.java
@@ -0,0 +1,31 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem.controllers.rest;
+
+import com.OlyMahmudMugdho.inventorymanagementsystem.mappers.impl.ProductMapper;
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.dto.ProductDto;
+import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Product;
+import com.OlyMahmudMugdho.inventorymanagementsystem.services.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ProductControllerRest {
+    private final ProductMapper productMapper;
+    private ProductService productService;
+    public ProductControllerRest(ProductService productService, ProductMapper productMapper) {
+        this.productService = productService;
+        this.productMapper = productMapper;
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        List<ProductDto> productDtos = productService.getAllProducts();
+        List<Product> products = new ArrayList<>();
+        productDtos.stream().map(p -> productMapper.mapFrom(p)).forEach(products::add);
+        return products;
+    }
+}
