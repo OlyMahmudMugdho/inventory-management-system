3b42e9d2c295c4703bdbc283f18f34be18ab9c44
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-16 12:30:01 +0600

    get category by id controller

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/rest/CategoryControllerRest.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/rest/CategoryControllerRest.java
@@ -1,14 +1,15 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.controllers.rest;
 
 import com.OlyMahmudMugdho.inventorymanagementsystem.mappers.impl.CategoryMapper;
-import com.OlyMahmudMugdho.inventorymanagementsystem.models.dto.CategoryDto;
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Category;
 import com.OlyMahmudMugdho.inventorymanagementsystem.services.CategoryService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/categories")
@@ -25,5 +26,11 @@ public class CategoryControllerRest {
     public List<Category> getAllCategories() {
         List<Category> categories = categoryService.getAllCategories();
         return categories;
+    }
+
+    @GetMapping("{id}")
+    public Optional<Category> getCategoryById(@PathVariable long id) {
+        Optional<Category> category = categoryService.getCategoryById(id);
+        return Optional.ofNullable(category.get());
     }
 }
