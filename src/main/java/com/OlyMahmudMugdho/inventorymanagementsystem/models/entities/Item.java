f96f27af0f71904982414e5dcf6e4d668c0a5dff
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-02 21:31:42 +0600

    ordered item entity update

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/entities/Item.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/entities/Item.java
@@ -1,7 +1,9 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.models.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +14,11 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Item {
     @Id
+    @Column(name = "item_id")
     private Long id;
+    @ManyToOne
+    private Order orderId;
     private String productId;
     private Double quantity;
+    private Float price;
 }
