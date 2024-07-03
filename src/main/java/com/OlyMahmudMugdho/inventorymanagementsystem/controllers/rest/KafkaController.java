e7b8cfac29e5c2e846eaf1fb8933da68ed1f8b9f
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-03 14:41:53 +0600

    bulk add controller

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/rest/KafkaController.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/controllers/rest/KafkaController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/kafka")
 public class KafkaController {
-    private KafkaService kafkaService;
+
+    private final KafkaService kafkaService;
 
     public KafkaController(KafkaService kafkaService) {
         this.kafkaService = kafkaService;
@@ -22,4 +25,11 @@ public class KafkaController {
         return kafkaService.getOrders();
     }
 
+    @GetMapping("/orders/bulk-add")
+    public Map<String,Boolean> bulkAddOrders(){
+        Map<String,Boolean> response = new HashMap<>();
+        this.kafkaService.addAllOrderToDb();
+        response.put("added", true);
+        return response;
+    }
 }
