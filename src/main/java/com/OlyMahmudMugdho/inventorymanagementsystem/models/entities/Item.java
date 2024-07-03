248e1c07a2e65cc57496c4758ac37de5be20df95
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-03 13:02:00 +0600

    Item entity

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/entities/Item.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/entities/Item.java
@@ -1,9 +1,6 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.models.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +12,7 @@ import lombok.NoArgsConstructor;
 public class Item {
     @Id
     @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @ManyToOne
     private Order orderId;
