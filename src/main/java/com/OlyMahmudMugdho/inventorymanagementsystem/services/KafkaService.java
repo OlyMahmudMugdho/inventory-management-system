2cef26264637850e2b1c1957301982b97d81d5dd
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-03 13:49:49 +0600

    get all service

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/services/KafkaService.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/services/KafkaService.java
@@ -2,7 +2,6 @@ package com.OlyMahmudMugdho.inventorymanagementsystem.services;
 
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.dto.OrderDto;
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Order;
-import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +10,21 @@ import java.util.List;
 
 @Service
 public class KafkaService {
-    private List<Order> orders = new ArrayList<>();
+    private List<OrderDto> orders = new ArrayList<>();
+
+
     @KafkaListener(topics = "orders",
             groupId = "listen1",
             containerFactory = "orderListener")
-    public void listenOrder(OrderDto message) {
+
+    public OrderDto listenOrder(OrderDto message) {
         System.out.println(message.toString());
+        orders.add(message);
+        return message;
     }
+
+    public List<OrderDto> getOrders() {
+        return orders;
+    }
+
 }
