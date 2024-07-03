c32cec0ed359dd5526d77f66b500e72bfe60ed6e
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-03 14:41:43 +0600

    bulk add orders

--- src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/services/KafkaService.java
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/services/KafkaService.java
@@ -1,16 +1,32 @@
 package com.OlyMahmudMugdho.inventorymanagementsystem.services;
 
+import com.OlyMahmudMugdho.inventorymanagementsystem.mappers.impl.OrderMapper;
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.dto.OrderDto;
 import com.OlyMahmudMugdho.inventorymanagementsystem.models.entities.Order;
+import com.OlyMahmudMugdho.inventorymanagementsystem.repositories.ItemRepository;
+import com.OlyMahmudMugdho.inventorymanagementsystem.repositories.OrderRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class KafkaService {
-    private List<OrderDto> orders = new ArrayList<>();
+
+    private OrderRepository orderRepository;
+    private ItemRepository itemRepository;
+    private OrderMapper orderMapper = new OrderMapper();
+    private List<Order> orders = new ArrayList<>();
+
+    public KafkaService(OrderRepository orderRepository, ItemRepository itemRepository) {
+        this.orderRepository = orderRepository;
+        this.itemRepository = itemRepository;
+    }
 
 
     @KafkaListener(topics = "orders",
@@ -19,12 +35,18 @@ public class KafkaService {
 
     public OrderDto listenOrder(OrderDto message) {
         System.out.println(message.toString());
-        orders.add(message);
+        orders.add(orderMapper.mapFrom(message));
         return message;
     }
 
     public List<OrderDto> getOrders() {
-        return orders;
+        return orders.stream().map(
+                o -> orderMapper.mapTo(o)
+        ).collect(Collectors.toList());
+    }
+
+    public void addAllOrderToDb() {
+        orderRepository.saveAll(orders);
     }
 
 }
