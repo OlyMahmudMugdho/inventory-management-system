04a7bea5e779eaf81febf147397aeb72b44dc7c7
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-03 13:02:29 +0600

    Order DTO

---
+++ src/main/java/com/OlyMahmudMugdho/inventorymanagementsystem/models/dto/OrderDto.java
@@ -0,0 +1,15 @@
+package com.OlyMahmudMugdho.inventorymanagementsystem.models.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDto {
+    private List<ItemDto> items;
+    private String userId;
+    private String message;
+}
